d657f6ad36c106a3696632b116ed2afd581ffb88
TorATB <37066848+TorATB@users.noreply.github.com>
2022-06-22 23:34:50 +0200

    Add files via upload

--- get date from filename and update correct info in json file.ps1
+++ get date from filename and update correct info in json file.ps1
@@ -1,9 +1,9 @@
 ﻿
-$MediaFolder = "D:\Backup\Cloud\Exported Google Photos - 2022.06.15"
+$MediaFolder = "C:\Temp\test3\jeje"
 $MyFiles = Get-ChildItem -Path $MediaFolder -File –Recurse -Exclude *.json,*.db,*.ps1,*.mp4_exiftool_tmp
 
 #Normal date format detecting regex:
-$regex1 = '(?<filedate1>\d{4}(?:\.|-|_)?\d{2}(?:\.|-|_)?\d{2}(?:\.|-|_)?\d{2}(?:\.|-|_)?\d{2}(?:\.|-|_)?\d{2})[^0-9]'
+$regex1 = '(?<filedate1>[0-3]\d{3}(?:\.|-|_)?\d{2}(?:\.|-|_)?\d{2})[^0-9]'
 
 #Unix timestamp date format detecting regex:
 $regex2 = '(?<filedate2>[0-2]\d{8}(?:\.|-|_)?\d{2}(?:\.|-|_)?\d{2})[^0-9]'
@@ -13,18 +13,20 @@ function Get-GoogleDateFormat {
         $InDate
     )
     $MyMonth = (get-date $InDate -Format Y).ToString()
-    $MyMonth = $MyDate.Substring(0, [Math]::Min($MyDate.Length, 3))
+    $MyMonth = $MyMonth.Substring(0, [Math]::Min($MyMonth.Length, 3))
     $MyDate = Get-Date $InDate -Format "dd, yyyy, HH:mm:ss"
     $MyDate = "$MyMonth $MyDate PM UTC"
     $MyDate
 }
 
+
 $MyFiles | ForEach-Object {
     $MyFullName = $_.FullName
+    $MyName = $_.Name
     If ($_.Name -match $regex1) {
         $date = ($Matches['filedate1'] -replace '(\.|-|_)','')
         try {
-            $dateFromFileName = [datetime]::ParseExact($date,'yyyyMMddHHmmss',[cultureinfo]::InvariantCulture)
+            $dateFromFileName = [datetime]::ParseExact($date,'yyyyMMdd',[cultureinfo]::InvariantCulture)
             $timestampFromFileName = Get-Date -Date $dateFromFileName -UFormat %s
             $dateFromFileNameString = $dateFromFileName.tostring("yyyy:MM:dd HH:mm:ss")
             
@@ -32,7 +34,9 @@ $MyFiles | ForEach-Object {
             $dateFromJson=(Get-Date 01.01.1970)+([System.TimeSpan]::fromseconds($json.photoTakenTime.Timestamp))
             $timestampFromJson = Get-Date -Date $dateFromJson -UFormat %s
 
-            if ((($dateFromJson - $dateFromFileName).Days) -gt 0) {
+            ($dateFromJson - $dateFromFileName).Days
+
+            if ((($dateFromJson - $dateFromFileName).Days) -ne 0) {
                 write-host "Found wrong date in json-file, setting correct date in $MyFullName.json"
                 $json.photoTakenTime.formatted = Get-GoogleDateFormat $dateFromFileName
                 $json.photoTakenTime.timestamp = $timestampFromFileName
@@ -42,9 +46,11 @@ $MyFiles | ForEach-Object {
                 #save json-file
                 $json | convertto-json | set-content "$MyFullName.json" -encoding utf8
             }
+            
         }catch {}
     } ElseIf ($_.Name -match $regex2) {
         $date = ($Matches['filedate2'] -replace '(\.|-|_)','').subString(0, 10)
+        $date
         try {
             $timestampFromFileName = $date
             $dateFromFileName = (Get-Date 01.01.1970)+([System.TimeSpan]::fromseconds($date))
