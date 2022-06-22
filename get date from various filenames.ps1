0039d4fae78199f0047a9ebab8e193baeb9b7a78
TorATB <37066848+TorATB@users.noreply.github.com>
2022-06-22 23:36:52 +0200

    Add files via upload

--- get date from various filenames.ps1
+++ get date from various filenames.ps1
@@ -5,7 +5,7 @@ $ExifToolLocation = "C:\Temp\exiftool-12.42\exiftool.exe"
 $MyFiles = Get-ChildItem -Path $MediaFolder -File –Recurse -Exclude *.json,*.db,*.ps1,*.mp4_exiftool_tmp
 
 #Normal date format detecting regex:
-$regex1 = '(?<filedate1>\d{4}(?:\.|-|_)?\d{2}(?:\.|-|_)?\d{2}(?:\.|-|_)?\d{2}(?:\.|-|_)?\d{2}(?:\.|-|_)?\d{2})[^0-9]'
+$regex1 = '(?<filedate1>[0-3]\d{3}(?:\.|-|_)?\d{2}(?:\.|-|_)?\d{2})[^0-9]'
 
 #Unix timestamp date format detecting regex:
 $regex2 = '(?<filedate2>[0-2]\d{8}(?:\.|-|_)?\d{2}(?:\.|-|_)?\d{2})[^0-9]'
