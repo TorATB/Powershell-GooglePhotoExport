14b6ea80f027daf9004907f5e1a9447b84278103
TorATB <37066848+TorATB@users.noreply.github.com>
2024-09-27 18:50:07 +0200

    Add files via upload

--- get date from various filenames.ps1
+++ get date from various filenames.ps1
@@ -1,7 +1,9 @@
 ﻿
+
 $i=0
-$MediaFolder = "D:\Backup\Cloud\Xiaomi 12 Pro 2022\Screenshots"
-$ExifToolLocation = "C:\Program Files\exiftool-12.42\exiftool.exe"
+$MediaFolder = "C:\Users\Tor\Downloads\immich-20240923_201559-10-sep\New folder"
+$ExifToolLocation = "C:\Users\Tor\Downloads\immich-20240923_201559-10-sep\exiftool.exe"
+Get-ChildItem $MediaFolder | Unblock-File
 $MyFiles = Get-ChildItem -Path $MediaFolder -File –Recurse -Exclude *.json,*.db,*.ps1,*.mp4_exiftool_tmp
 
 #Normal date format detecting regex:
@@ -9,6 +11,13 @@ $regex1 = '(?<filedate1>[0-3]\d{3}(?:\.|-|_)?\d{2}(?:\.|-|_)?\d{2})[^0-9]'
 
 #Unix timestamp date format detecting regex:
 $regex2 = '(?<filedate2>[0-2]\d{8}(?:\.|-|_)?\d{2}(?:\.|-|_)?\d{2})[^0-9]'
+
+#Next level of regex is trying to find if there is a clock time after the date
+$regex3 = '(?<clocktime1>[0-2]\d(?:\.|-|_)?[0-5]\d(?:\.|-|_)?[0-5]\d)'
+
+#Next level of regex is trying to find if there is a clock time after the date
+$regex4 = '(?<clocktime2>[0-2]?[0-9][0-3][0-5][0-9])'
+#^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$
 
 $MyFiles | ForEach-Object {
     $MyFullName = $_.FullName
@@ -18,19 +27,25 @@ $MyFiles | ForEach-Object {
         $date = ($Matches['filedate1'] -replace '(\.|-|_)','')
         try {
             $dateFromFileName = [datetime]::ParseExact($date,'yyyyMMdd',[cultureinfo]::InvariantCulture)
-
-            #Next level of regex is trying to find if there is a clock time after the date
-            $regex3 = '(?<clocktime>[0-2]\d(?:\.|-|_)?[0-5]\d(?:\.|-|_)?[0-5]\d)'
             $MyName = $MyName.Replace($MyMatch,'')
-
             If ($MyName -match $regex3) {
-                $clocktime = ($Matches['clocktime'] -replace '(\.|-|_)','')
+                $clocktime = ($Matches['clocktime1'] -replace '(\.|-|_)','')
                 try {
                     $dateFromFileName = $dateFromFileName.AddHours($clocktime.Substring(0,2))
                     $dateFromFileName = $dateFromFileName.AddMinutes($clocktime.Substring(2,2))
                     $dateFromFileName = $dateFromFileName.AddSeconds($clocktime.Substring(4,2))
                     #Correcting timeclock if found
-                    $dateString = $dateFromFileName.tostring("yyyy:MM:dd HH:mm:ss")
+                    $dateString = $dateFromFileName.tostring("yyyy/MM/dd HH:mm:ss")
+                }catch {}
+            }
+            ElseIf ($MyName -match $regex4) {
+            $clocktime = ($Matches['clocktime2'] -replace '')
+                try {
+                    $dateFromFileName = $dateFromFileName.AddHours($clocktime.Substring(0,2))
+                    $dateFromFileName = $dateFromFileName.AddMinutes($clocktime.Substring(2,2))
+                    $dateFromFileName = $dateFromFileName.AddSeconds(00)
+                    #Correcting timeclock if found
+                    $dateString = $dateFromFileName.tostring("yyyy/MM/dd HH:mm:ss")
                 }catch {}
             }
 
@@ -53,3 +68,5 @@ $MyFiles | ForEach-Object {
     }
 }
 $i
+
+
